92aa372b14fd1253fc77ca20ac63aa42639670a1
abdullah.al-omari <abdullah10182@gmail.com>
2019-08-14 16:10:56 -0400

    data being passed to other activity

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,49 @@
+
 package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject sandwichData = new JSONObject(json);
+            JSONObject sandwichNameObj = sandwichData.getJSONObject("name");
+            String mainName = sandwichNameObj.getString("mainName");
+            JSONArray alsoKnownAsArray = sandwichNameObj.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<String>();
+            for(int i = 0; i < alsoKnownAsArray.length(); i++) {
+                alsoKnownAs.add(alsoKnownAsArray.getString(i));
+            }
+            String placeOfOrigin = sandwichData.getString("placeOfOrigin");
+            String description = sandwichData.getString("description");
+            String image = sandwichData.getString("image");
+            JSONArray ingredientsArray = sandwichData.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<String>();
+            for(int i = 0; i < ingredientsArray.length(); i++) {
+                ingredients.add(ingredientsArray.getString(i));
+            }
+            Sandwich sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients );
+            //public Sandwich(String mainName, List<String> alsoKnownAs, String placeOfOrigin, String description, String image, List<String> ingredients)
+            return sandwich;
+        } catch ( JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
+//public class JsonUtils {
+//
+//    public static Sandwich parseSandwichJson(String json) {
+//        JSONObject sandwichData = new JSONObject(json);
+//        JSONObject weather = forecast.getJSONObject("weather");
+//        return weather.getString("condition");
+//    }
+//}
